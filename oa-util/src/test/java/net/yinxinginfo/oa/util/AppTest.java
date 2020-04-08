1864969824dc28071046f21046082f1fbbe65379
白西郊 <492681225@qq.com>
2020-04-08 14:36:01 +0800

    oa项目结构

---
+++ oa-util/src/test/java/net/yinxinginfo/oa/util/AppTest.java
@@ -0,0 +1,20 @@
+package net.yinxinginfo.oa.util;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ oa-web/src/main/java/net/yinxinginfo/oa/web/DemoAction.java
@@ -0,0 +1,10 @@
+package net.yinxinginfo.oa.web;
+
+/**
+ * @Auther: baixijiao
+ * @Date：2020-04-08 10:35
+ * @Description：
+ * @Modified By:
+ */
+public class DemoAction {
+}
